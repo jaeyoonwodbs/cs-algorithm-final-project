059b1c7a09cc6453f6b4b7529f0c246c4a1a4748
Jaeyoon Yu <dmstn3431@naver.com>
2023-12-06 13:01:22 +0900

    날짜 입력 -> 기간 입력 수정

--- BinarySearch.py
+++ BinarySearch.py
@@ -5,65 +5,72 @@ class Hash:
     def push(self, key, value1, value2):
         if key in self.hash_table:
             self.hash_table[key].append([value1, value2])
-            self.hash_table[key] = sorted(self.hash_table[key], key=lambda x: x[1])  # Sort entries based on category
         else:
             self.hash_table[key] = [[value1, value2]]
 
     def get_values(self, key):
         return self.hash_table[key]
 
-    def binary_search(self, entries, target_category):
-        low, high = 0, len(entries) - 1
-        cumulative_sum = 0  # To keep track of the cumulative sum
-
-        while low <= high:
-            mid = (low + high) // 2
-            mid_category = entries[mid][1]
-
-            if mid_category == target_category:
-                # Calculate cumulative sum for the found category
-                for i in range(mid + 1):
-                    cumulative_sum += entries[i][0]
-                return cumulative_sum
-            elif mid_category < target_category:
-                low = mid + 1
+    def binary_search(self, keys, target, start, end):
+        while start <= end:
+            mid = (start + end) // 2
+            if keys[mid] == target:
+                return mid
+            elif keys[mid] < target:
+                start = mid + 1
             else:
-                high = mid - 1
+                end = mid - 1
+        return start
 
-        return 0  # Return 0 if the category is not found
+    def search_range(self, target_start, target_end):
+        result = {}
+        original_entries = []
+
+        keys = sorted(self.hash_table.keys())  # Ensure the keys are sorted
+
+        # Binary search for the start and end indices
+        start_index = self.binary_search(keys, target_start, 0, len(keys) - 1)
+        end_index = self.binary_search(keys, target_end, 0, len(keys) - 1)
+
+        for key in keys[start_index : end_index + 1]:
+            entries = self.hash_table[key]
+            original_entries.extend(entries)
+            for entry in entries:
+                category = entry[1]
+                expenditure = entry[0]
+                if category in result:
+                    result[category] += expenditure
+                else:
+                    result[category] = expenditure
+
+        return original_entries, result
 
 import pandas as pd
 
 def main():
     hash_table = Hash()
-    
-    headers=['date','expenditure','category']
+
+    headers = ['date', 'expenditure', 'category']
     df = pd.read_csv('account_book.csv', names=headers)
-    
-    # 해시테이블 생성: 1개의 key값에 여러개의 value값 갖게 함. (날짜, 금액, 카테고리) 순으로 삽입
+
     for index, row in df.iterrows():
         hash_table.push(str(row['date']), int(row['expenditure']), int(row['category']))
-        
-    target=str(input("날짜 입력: "))
 
-    if target in hash_table.hash_table:
-        original_entries = hash_table.hash_table[target]
-        
+    target_start = str(input("시작 날짜 입력: "))
+    target_end = str(input("종료 날짜 입력: "))
+
+    original_entries, cumulative_expenditures = hash_table.search_range(target_start, target_end)
+
+    if original_entries:
         print("-----지출 내역------")
         for entry in original_entries:
             print(f"지출금액(원): {entry[0]}, 카테고리: {entry[1]}")
-        
+
         print("\n-----누적 지출금액-----")
-        cumulative_expenditures = {}
-        for entry in original_entries:
-            category = entry[1]
-            cumulative_expenditure = hash_table.binary_search(original_entries, category)
-            cumulative_expenditures[category] = cumulative_expenditure
-        
         for category, cumulative_expenditure in cumulative_expenditures.items():
             print(f"카테고리: {category}, 누적 지출금액(원): {cumulative_expenditure}")
     else:
-        print("해당 날짜의 지출이 없습니다.")
+        print("해당 기간의 지출이 없습니다.")
 
 if __name__ == "__main__":
     main()
--- SequentialSearch.py
+++ SequentialSearch.py
@@ -11,46 +11,48 @@ class Hash:
     def get_values(self, key):
         return self.hash_table[key]
 
-    def linear_search(self, target):
-        if target in self.hash_table:
-            original_entries = self.hash_table[target]
-            result = {}
-            for entry in original_entries:
-                category = entry[1]
-                expenditure = entry[0]
-                if category in result:
-                    result[category] += expenditure
-                else:
-                    result[category] = expenditure
-            return original_entries, result
-        return None, None
+    def sequential_search(self, target_start, target_end):
+        result = {}
+        original_entries = []
+        for key in self.hash_table:
+            if target_start <= key <= target_end:
+                entries = self.hash_table[key]
+                original_entries.extend(entries)
+                for entry in entries:
+                    category = entry[1]
+                    expenditure = entry[0]
+                    if category in result:
+                        result[category] += expenditure
+                    else:
+                        result[category] = expenditure
+        return original_entries, result
 
 import pandas as pd
 
 def main():
     hash_table = Hash()
-    
-    headers=['date','expenditure','category']
+
+    headers = ['date', 'expenditure', 'category']
     df = pd.read_csv('account_book.csv', names=headers)
-    
-    # 해시테이블 생성: 1개의 key값에 여러개의 value값 갖게 함. (날짜, 금액, 카테고리) 순으로 삽입
+
     for index, row in df.iterrows():
         hash_table.push(str(row['date']), int(row['expenditure']), int(row['category']))
-        
-    target = str(input("날짜 입력: "))
 
-    original_entries, cumulative_expenditures = hash_table.linear_search(target)
+    target_start = str(input("시작 날짜 입력: "))
+    target_end = str(input("종료 날짜 입력: "))
 
-    if original_entries is not None:
+    original_entries, cumulative_expenditures = hash_table.sequential_search(target_start, target_end)
+
+    if original_entries:
         print("-----지출 내역------")
         for entry in original_entries:
             print(f"지출금액(원): {entry[0]}, 카테고리: {entry[1]}")
-        
+
         print("\n-----누적 지출금액-----")
         for category, cumulative_expenditure in cumulative_expenditures.items():
             print(f"카테고리: {category}, 누적 지출금액(원): {cumulative_expenditure}")
     else:
-        print("해당 날짜의 지출이 없습니다.")
+        print("해당 기간의 지출이 없습니다.")
 
 if __name__ == "__main__":
     main()
