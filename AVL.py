d4a12f7f00e9a6a3435bf22c6b579be9663fc5bd
Jaeyoon Yu <dmstn3431@naver.com>
2023-12-06 14:59:54 +0900

    avl 날짜 입력 코드 추가

--- AVL.py
+++ AVL.py
@@ -1,91 +1,126 @@
 import pandas as pd
 
-class Node:
-    def __init__(self, key, height, left=None, right=None):
+class AVLNode:
+    def __init__(self, key, value, left=None, right=None):
         self.key = key
-        self.height = height
+        self.value = [value]  # Store a list of values for the same key
         self.left = left
         self.right = right
+        self.height = 1
 
-class AVL:
+class AVLTree:
     def __init__(self):
         self.root = None
-    def height(self, n):
-        if n == None:
+
+    def height(self, node):
+        if not node:
             return 0
-        return n.height
+        return node.height
 
-    def put(self, key):
-        self.root = self.put_item(self.root, key)
+    def update_height(self, node):
+        if not node:
+            return 0
+        node.height = 1 + max(self.height(node.left), self.height(node.right))
 
-    def put_item(self, n, key):
-        if n == None:
-            return  Node(key, 1)
-        if n.key > key:
-            n.left = self.put_item(n.left, key)
-        elif n.key < key:
-            n.right = self.put_item(n.right, key)
-        n.height = max(self.height(n.left), self.height(n.right))+1
-        return self.balance(n)
+    def balance_factor(self, node):
+        if not node:
+            return 0
+        return self.height(node.left) - self.height(node.right)
 
-    def balance(self, n):
+    def left_rotate(self, z):
+        y = z.right
+        T2 = y.left
 
-        if self.bf(n) > 1: # 왼쪽 서브트리 불균형
-            if self.bf(n.left) < 0: # 노드 n의 왼쪽 자식의 오른쪽 서브트리가 높은 경우
-                n.left = self.rotate_left(n.left) # LR
-            n = self.rotate_right(n) # LL
+        y.left = z
+        z.right = T2
 
-        elif self.bf(n) < -1: # 오른쪽 서브트리 불균형
-            if self.bf(n.right) > 0: # 노드 n의 오른쪽 자식의 왼쪽 서브트리가 높은 경우
-                n.right = self.rotate_right(n.right) #RL
-            n = self.rotate_left(n) #RR
-        return n
+        self.update_height(z)
+        self.update_height(y)
 
-    def bf(self, n):
-        return self.height(n.left)-self.height(n.right)
+        return y
 
-    def rotate_right(self, n): #오른쪽 회전
-        x = n.left
-        n.left = x.right
-        x.right = n
-        n.height = max(self.height(n.left), self.height(n.right)) + 1
-        x.height = max(self.height(x.left), self.height(x.right)) + 1
+    def right_rotate(self, y):
+        x = y.left
+        T2 = x.right
+
+        x.right = y
+        y.left = T2
+
+        self.update_height(y)
+        self.update_height(x)
+
         return x
 
-    def rotate_left(self, n): #왼쪽 회전
-        x = n.right
-        n.right = x.left
-        x.left = n
-        n.height = max(self.height(n.left), self.height(n.right)) + 1
-        x.height = max(self.height(x.left), self.height(x.right)) + 1
-        return x
+    def insert(self, root, key, value):
+        if not root:
+            return AVLNode(key, value)
 
-    def preorder(self, n):
-        print(str(n.key),' ', end="")
-        if n.left:
-            self.preorder(n.left)
-        if n.right:
-            self.preorder(n.right)
-    def inorder(self, n):
-        if n.left:
-            self.inorder(n.left)
-        print(str(n.key), ' ', end="")
-        if n.right:
-            self.inorder(n.right)
+        if key < root.key:
+            root.left = self.insert(root.left, key, value)
+        elif key > root.key:
+            root.right = self.insert(root.right, key, value)
+        else:
+            # Duplicate keys are allowed, store multiple values in a list
+            root.value.append(value)
+            return root
+
+        self.update_height(root)
+
+        # Check and balance the tree
+        balance = self.balance_factor(root)
+
+        # Left Left Case
+        if balance > 1 and key < root.left.key:
+            return self.right_rotate(root)
+
+        # Right Right Case
+        if balance < -1 and key > root.right.key:
+            return self.left_rotate(root)
+
+        # Left Right Case
+        if balance > 1 and key > root.left.key:
+            root.left = self.left_rotate(root.left)
+            return self.right_rotate(root)
+
+        # Right Left Case
+        if balance < -1 and key < root.right.key:
+            root.right = self.right_rotate(root.right)
+            return self.left_rotate(root)
+
+        return root
+
+    def insert_node(self, key, value):
+        self.root = self.insert(self.root, key, value)
+
+    def search(self, root, key):
+        if not root or root.key == key:
+            return root
+        if key < root.key:
+            return self.search(root.left, key)
+        return self.search(root.right, key)
+
+    def search_by_key(self, key):
+        node = self.search(self.root, key)
+        return node.value if node else None
 
 def main():
-    avl = AVL()
+    avl = AVLTree()
 
-    headers=['date','expenditure','category']
-    df=pd.read_csv('account_book.csv',names=headers)
-    
+    headers = ['date', 'expenditure', 'category']
+    df = pd.read_csv('account_book.csv', names=headers)
+
     for index, row in df.iterrows():
-        avl.put(int(row['expenditure']))
-    
-    avl.inorder(avl.root)
-    print("\n")
+        avl.insert_node(str(row['date']), (int(row['expenditure']), int(row['category'])))
 
-    #수정예정!!
+    target = str(input("시작날짜 입력: "))
 
-if __name__=="__main__":
+    result = avl.search_by_key(target)
+
+    if result:
+        for value in result:
+            print(f"비용: {value[0]}, 카테고리: {value[1]}")
+    else:
+        print(f"{target}에 해당하는 데이터가 없습니다.")
+
+if __name__ == "__main__":
     main()
